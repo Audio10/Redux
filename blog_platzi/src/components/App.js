0d2369f8ceed5493bfb1f07abf588f90f6c38d26
Claudio Jesus Vazquez Villanueva <claudiojesus_10@hotmail.com>
2019-09-22 13:22:16 -0500

    finished 4. Redux

--- blog_platzi/src/components/App.js
+++ blog_platzi/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Menu from "./Menu"
 import Usuarios from './Usuarios'
 
@@ -9,8 +9,10 @@ const Tareas = () => <div> Tareas </div>
 const App = () => (
   <BrowserRouter>
     <Menu />
-    <Route exact path="/" component={Usuarios} />
-    <Route exact path="/tareas" component={Tareas} />
+    <div className="margen">
+      <Route exact path="/" component={Usuarios} />
+      <Route exact path="/tareas" component={Tareas} />
+    </div>
   </BrowserRouter>
 );
 
